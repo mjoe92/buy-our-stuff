bce92f62f9399d3ad6227167ddf03683e3684dd2
DurayBence <duray.bence@users.noreply.github.com>
2021-03-03 09:25:33 +0100

    Add test classes

---
+++ src/test/java/com/codecool/buyourstuff/dao/UserDaoTest.java
@@ -0,0 +1,14 @@
+package com.codecool.buyourstuff.dao;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserDaoTest {
+
+    @Test
+    void doSomeTesting() {
+        assertTrue(3 == 3);
+    }
+
+}
