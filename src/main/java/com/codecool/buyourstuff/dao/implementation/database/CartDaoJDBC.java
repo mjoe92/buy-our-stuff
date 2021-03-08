18248a088f0936958ee4c70d642534c9fc3e8eef
huszarpp <huszarpp@gmail.com>
2021-03-08 22:52:52 +0100

    Create CartDaoJDBC

--- src/main/java/com/codecool/buyourstuff/dao/implementation/database/CartDaoJDBC.java
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/database/CartDaoJDBC.java
@@ -4,11 +4,16 @@ import com.codecool.buyourstuff.dao.CartDao;
 import com.codecool.buyourstuff.model.Cart;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CartDaoJDBC implements CartDao {
 
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
     public CartDaoJDBC(DataSource dataSource) {
         this.dataSource = dataSource;
@@ -17,32 +22,85 @@ public class CartDaoJDBC implements CartDao {
 
     @Override
     public void createTable() {
-
+        String sql = "CREATE TABLE IF NOT EXISTS cart(" +
+                "id SERIAL PRIMARY KEY, " +
+                "currency TEXT NOT NULL);";
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement pst = connection.prepareStatement(sql);
+            pst.execute();
+        } catch (SQLException sqle) {
+            throw new RuntimeException(getClass().getSimpleName() + " " + sql + ": " + sqle.getSQLState());
+        }
     }
 
     @Override
     public void add(Cart cart) {
-
+        String sql = "INSERT INTO cart VALUES (?);";
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement pst = connection.prepareStatement(sql);
+            pst.setString(1, cart.getCurrency().getCurrencyCode());
+            pst.executeUpdate();
+        } catch (SQLException sqle) {
+            throw new RuntimeException(getClass().getSimpleName() + " " + sql + ": " + sqle.getSQLState());
+        }
     }
 
     @Override
     public Cart find(int id) {
-        return null;
+        Cart cart = null;
+        String sql = "SELECT currency FROM cart WHERE id = ?;";
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement pst = connection.prepareStatement(sql);
+            pst.setInt(1, id);
+            ResultSet resultSet = pst.executeQuery();
+            if (resultSet.next()) {
+                cart = new Cart(resultSet.getString("currency"));
+                cart.setId(id);
+            }
+        } catch (SQLException sqle) {
+            throw new RuntimeException(getClass().getSimpleName() + " " + sql + ": " + sqle.getSQLState());
+        }
+            return cart;
     }
 
     @Override
     public void remove(int id) {
-
+        String sql = "DELETE FROM cart WHERE id = ?;";
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement pst = connection.prepareStatement(sql);
+            pst.setInt(1, id);
+            pst.executeUpdate();
+        }  catch (SQLException sqle) {
+            throw new RuntimeException(getClass().getSimpleName() + " " + sql + ": " + sqle.getSQLState());
+        }
     }
 
     @Override
     public void clear() {
-
+        String sql = "DELETE * FROM cart;";
+        try (Connection connection = dataSource.getConnection()) {
+            connection.prepareStatement(sql).executeUpdate();
+        }  catch (SQLException sqle) {
+            throw new RuntimeException(getClass().getSimpleName() + " " + sql + ": " + sqle.getSQLState());
+        }
     }
 
     @Override
     public List<Cart> getAll() {
-        return null;
+        List<Cart> cartList;
+        String sql = "SELECT currency FROM cart;";
+        try (Connection connection = dataSource.getConnection()) {
+            ResultSet resultSet = connection.prepareStatement(sql).executeQuery();
+            cartList = new ArrayList<>();
+            Cart cart;
+            while (resultSet.next()) {
+                cart = new Cart(resultSet.getString("currency"));
+                cart.setId(resultSet.getInt("id"));
+                cartList.add(cart);
+            }
+        } catch (SQLException sqle) {
+            throw new RuntimeException(getClass().getSimpleName() + " " + sql + ": " + sqle.getSQLState());
+        }
+        return cartList;
     }
-
 }
