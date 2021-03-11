c04e5e8361aa77ecf4d3536c623e99f74d10f837
huszarpp <huszarpp@gmail.com>
2021-03-11 18:05:43 +0100

    Implement Deserializer

---
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/file/serializer/Deserializer.java
@@ -0,0 +1,53 @@
+package com.codecool.buyourstuff.dao.implementation.file.serializer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deserializer<T> {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final String filename;
+    private final Class<T> type;
+
+    public Deserializer(String filename, Class<T> type) {
+        this.filename = filename;
+        this.type = type;
+    }
+    public List<T> deserializeAll() {
+        List<T> deserializedList = new ArrayList<>();
+        try {
+            List<String> JSONList = readFile();
+            for (String JSONItem : JSONList) {
+                deserializedList.add(mapper.readValue(JSONItem, this.type));
+            }
+        } catch (JsonProcessingException JE) {
+            System.out.println(JE.getMessage());
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+        return deserializedList;
+    }
+
+    private List<String> readFile() throws IOException {
+        List<String> JSONList = new ArrayList<>();
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(this.filename));
+            String nextLine = reader.readLine();
+            while (nextLine != null) {
+                JSONList.add(nextLine);
+                nextLine = reader.readLine();
+            }
+            reader.close();
+        }  catch (IOException IOE) {
+            throw new IOException("Error while reading", IOE);
+        }
+        return JSONList;
+    }
+}
