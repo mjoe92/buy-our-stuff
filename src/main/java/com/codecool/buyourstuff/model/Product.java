bf8cb4d25c9eac3bfdd244b74d43c4b0a4d98f62
huszarpp <huszarpp@gmail.com>
2021-03-11 19:56:19 +0100

    Add NoArgsConstructor and JSONIgnore to Product

--- src/main/java/com/codecool/buyourstuff/model/Product.java
+++ src/main/java/com/codecool/buyourstuff/model/Product.java
@@ -1,6 +1,8 @@
 package com.codecool.buyourstuff.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Setter;
 
@@ -8,6 +10,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Currency;
 
+@NoArgsConstructor
 @Getter
 @Setter
 public class Product extends BaseModel {
@@ -32,9 +35,11 @@ public class Product extends BaseModel {
     public BigDecimal getDefaultPrice(int decimals) {
         return defaultPrice.setScale(decimals, RoundingMode.HALF_DOWN);
     }
+    @JsonIgnore
     public String getPrice() {
         return getDefaultPrice(2) + " " + defaultCurrency.toString();
     }
+    @JsonIgnore
     public void setPrice(BigDecimal price, String currency) {
         this.defaultPrice = price;
         this.defaultCurrency = Currency.getInstance(currency);
