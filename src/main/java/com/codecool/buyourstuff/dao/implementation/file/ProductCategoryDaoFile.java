5094246695821c5feff75ad2bbc33b29077d58dd
huszarpp <huszarpp@gmail.com>
2021-03-11 18:51:14 +0100

    Add deserialization to field, constructor, methods, idCounter to ProductCategoryDaoFile

--- src/main/java/com/codecool/buyourstuff/dao/implementation/file/ProductCategoryDaoFile.java
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/file/ProductCategoryDaoFile.java
@@ -1,6 +1,7 @@
 package com.codecool.buyourstuff.dao.implementation.file;
 
 import com.codecool.buyourstuff.dao.ProductCategoryDao;
+import com.codecool.buyourstuff.dao.implementation.file.serializer.Deserializer;
 import com.codecool.buyourstuff.dao.implementation.file.serializer.Serializer;
 import com.codecool.buyourstuff.model.ProductCategory;
 import com.codecool.buyourstuff.model.exception.DataNotFoundException;
@@ -13,12 +14,16 @@ import java.util.List;
 public class ProductCategoryDaoFile implements ProductCategoryDao {
 
     private List<ProductCategory> productCategoriesMemo = new ArrayList<>();
+    private int idCounter;
 
     private final String url = "src/main/resources/product_category.json";
-    private final Serializer<ProductCategory> serializer = new Serializer(url);
+    private final Serializer<ProductCategory> serializer = new Serializer<ProductCategory>(url);
+    private final Deserializer<ProductCategory> deserializer = new Deserializer<ProductCategory>(url, ProductCategory.class);
 
     public ProductCategoryDaoFile() {
         createTable();
+        loadFileDataToMemory();
+        setIdCounter();
     }
 
     @Override
@@ -31,9 +36,22 @@ public class ProductCategoryDaoFile implements ProductCategoryDao {
         }
     }
 
+    private void loadFileDataToMemory() {
+        productCategoriesMemo.clear();
+        productCategoriesMemo = deserializer.deserializeAll();
+    }
+
+    private void setIdCounter() {
+        if (productCategoriesMemo.size() == 0) {
+            idCounter = 1;
+        } else {
+            idCounter = productCategoriesMemo.get(productCategoriesMemo.size() - 1).getId() + 1;
+        }
+    }
+
     @Override
     public ProductCategory add(ProductCategory category) {
-        category.setId(productCategoriesMemo.size() + 1);
+        category.setId(idCounter++);
 //        data.add(category);
         serializer.serializeOne(category);
         return category;
@@ -41,6 +59,7 @@ public class ProductCategoryDaoFile implements ProductCategoryDao {
 
     @Override
     public ProductCategory find(int id) {
+        loadFileDataToMemory();
         return productCategoriesMemo
                 .stream()
                 .filter(t -> t.getId() == id)
@@ -50,25 +69,30 @@ public class ProductCategoryDaoFile implements ProductCategoryDao {
 
     @Override
     public ProductCategory findByName(String name) {
+        loadFileDataToMemory();
         return productCategoriesMemo
                 .stream()
-                .filter(t -> t.getName() == name)
+                .filter(t -> t.getName().equals(name))
                 .findFirst()
                 .orElseThrow(() -> new DataNotFoundException("No such category"));
     }
 
     @Override
     public void remove(int id) {
+        loadFileDataToMemory();
         productCategoriesMemo.remove(find(id));
+        serializer.serializeAll(productCategoriesMemo);
     }
 
     @Override
     public void clear() {
         productCategoriesMemo = new ArrayList<>();
+        serializer.serializeAll(productCategoriesMemo);
     }
 
     @Override
     public List<ProductCategory> getAll() {
+        loadFileDataToMemory();
         return productCategoriesMemo;
     }
 }
