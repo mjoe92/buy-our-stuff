7b368793e4850814f107ee00881df26e41ed0153
huszarpp <huszarpp@gmail.com>
2021-03-11 22:29:30 +0100

    Compare id-s and not objects in getBy() methods

--- src/main/java/com/codecool/buyourstuff/dao/implementation/file/ProductDaoFile.java
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/file/ProductDaoFile.java
@@ -93,7 +93,7 @@ public class ProductDaoFile implements ProductDao {
         return productsMemo
                 .stream()
                 .filter(
-                        t -> t.getSupplier().equals(supplier)
+                        t -> t.getSupplier().getId() == (supplier.getId())
                 )
                 .collect(Collectors.toList());
     }
@@ -104,7 +104,7 @@ public class ProductDaoFile implements ProductDao {
         return productsMemo
                 .stream()
                 .filter(
-                        t -> t.getProductCategory().equals(productCategory)
+                        t -> t.getProductCategory().getId() == (productCategory.getId())
                 )
                 .collect(Collectors.toList());
     }
