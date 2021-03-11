23f6f4045c024177254a12559e51f2b68f0248ac
huszarpp <huszarpp@gmail.com>
2021-03-11 19:55:58 +0100

    Add NoArgsConstructor to ProductCategory

--- src/main/java/com/codecool/buyourstuff/model/ProductCategory.java
+++ src/main/java/com/codecool/buyourstuff/model/ProductCategory.java
@@ -1,10 +1,8 @@
 package com.codecool.buyourstuff.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
+import lombok.*;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
