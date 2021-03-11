7a6238d6671f8ba934e25b491ccbee0ee8866e90
huszarpp <huszarpp@gmail.com>
2021-03-11 16:00:30 +0100

    Add SupplierDaoFile fields, constructor, createTable(), add()

--- src/main/java/com/codecool/buyourstuff/dao/implementation/file/SupplierDaoFile.java
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/file/SupplierDaoFile.java
@@ -1,25 +1,40 @@
 package com.codecool.buyourstuff.dao.implementation.file;
 
 import com.codecool.buyourstuff.dao.SupplierDao;
+import com.codecool.buyourstuff.dao.implementation.file.serializer.Serializer;
 import com.codecool.buyourstuff.model.Supplier;
 import com.codecool.buyourstuff.model.exception.DataNotFoundException;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SupplierDaoFile implements SupplierDao {
 
+    private final String url = "src/main/resources/supplier.json";
+    private final Serializer<Supplier> serializer = new Serializer(url);
     private List<Supplier> data = new ArrayList<>();
+
+    public SupplierDaoFile() {
+        createTable();
+    }
 
     @Override
     public void createTable() {
-
+        try {
+            File file = new File(url);
+            file.createNewFile();       // doesn't do anything if file exists
+        } catch (IOException ioe) {
+            System.out.println("Exception while creating" + url);
+        }
     }
 
     @Override
     public Supplier add(Supplier supplier) {
         supplier.setId(data.size() + 1);
         data.add(supplier);
+        serializer.serializeOne(supplier);
         return supplier;
     }
 
