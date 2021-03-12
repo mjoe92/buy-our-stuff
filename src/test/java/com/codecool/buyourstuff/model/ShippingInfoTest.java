64eaf2b6a858f48e9f01b2d156d64ac117eb146d
DurayBence <duray.bence@users.noreply.github.com>
2021-03-12 09:19:51 +0100

    Update ShippingInfoTest

--- src/test/java/com/codecool/buyourstuff/model/ShippingInfoTest.java
+++ src/test/java/com/codecool/buyourstuff/model/ShippingInfoTest.java
@@ -5,9 +5,65 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ShippingInfoTest {
 
+    String NAME_DEFAULT = "Mohandas Karamchand Gandhi";
+    String EMAIL_DEFAULT = "AtomicGandhi1869@hotmail.com";
+    String PHONE_DEFAULT = "(91)235-95-139";
+    String ADDRESS_DEFAULT = "4215/1 Ansari Road, Daryaganj, New Delhi, Delhi, 110002, India";
+    ShippingInfo SI_DEFAULT = new ShippingInfo(NAME_DEFAULT, EMAIL_DEFAULT, PHONE_DEFAULT, ADDRESS_DEFAULT, ADDRESS_DEFAULT);
+
     @Test
-    void test() {
-        assertTrue(3 == 3);
+    void successfulCreation() {
+        assertNotNull(SI_DEFAULT);
+    }
+
+    /*
+    @Test
+    void invalidNameThrows() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new ShippingInfo(NAME_DEFAULT, EMAIL_DEFAULT, PHONE_DEFAULT, ADDRESS_DEFAULT, ADDRESS_DEFAULT));
+    }
+    */
+    @Test
+    void invalidEmailRegexThrows() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new ShippingInfo(NAME_DEFAULT, "wrongemail.com@imstillwrong", PHONE_DEFAULT, ADDRESS_DEFAULT, ADDRESS_DEFAULT));
+    }
+    @Test
+    void invalidPhoneNumberRegexThrows() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new ShippingInfo(NAME_DEFAULT, EMAIL_DEFAULT, "123-INVALID-456", ADDRESS_DEFAULT, ADDRESS_DEFAULT));
+    }
+    @Test
+    void invalidAddressRegexThrows() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new ShippingInfo(NAME_DEFAULT, EMAIL_DEFAULT, PHONE_DEFAULT, "Invalid St 123, Thingamajig County, Nowhere AS 123 GH, Non-existent Kingdom", ADDRESS_DEFAULT));
+    }
+
+
+    @Test
+    void lombokNullNameThrows() {
+        assertThrows(NullPointerException.class, () ->
+                new ShippingInfo(null, SI_DEFAULT.getEmail(), SI_DEFAULT.getPhoneNumber(), SI_DEFAULT.getBillingAddress(), SI_DEFAULT.getShippingAddress()));
+    }
+    @Test
+    void lombokNullEmailThrows() {
+        assertThrows(NullPointerException.class, () ->
+                new ShippingInfo(SI_DEFAULT.getEmail(), null, SI_DEFAULT.getPhoneNumber(), SI_DEFAULT.getBillingAddress(), SI_DEFAULT.getShippingAddress()));
+    }
+    @Test
+    void lombokNullPhoneNumberThrows() {
+        assertThrows(NullPointerException.class, () ->
+                new ShippingInfo(SI_DEFAULT.getEmail(), SI_DEFAULT.getEmail(), null, SI_DEFAULT.getBillingAddress(), SI_DEFAULT.getShippingAddress()));
+    }
+    @Test
+    void lombokNullBillingAddressThrows() {
+        assertThrows(NullPointerException.class, () ->
+                new ShippingInfo(SI_DEFAULT.getEmail(), SI_DEFAULT.getEmail(), SI_DEFAULT.getPhoneNumber(), null, SI_DEFAULT.getShippingAddress()));
+    }
+    @Test
+    void lombokNullShippingAddressThrows() {
+        assertThrows(NullPointerException.class, () ->
+                new ShippingInfo(SI_DEFAULT.getEmail(), SI_DEFAULT.getEmail(), SI_DEFAULT.getPhoneNumber(), SI_DEFAULT.getBillingAddress(), null));
     }
 
 }
